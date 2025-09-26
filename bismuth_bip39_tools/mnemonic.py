b99dddad3f11c37abbfdbc20e07e0d267c3da555
lama1 <you@example.com>
2025-09-26 17:52:20 +0900

    24 words mnemonic as a default

--- bismuth_bip39_tools/mnemonic.py
+++ bismuth_bip39_tools/mnemonic.py
@@ -1,10 +1,10 @@
 from mnemonic import Mnemonic
 
-def generate_mnemonic(strength: int = 128) -> str:
+def generate_mnemonic(strength: int = 256) -> str:
     """Generates a BIP39 mnemonic.
 
     Args:
-        strength: The strength of the mnemonic in bits. Can be 128 or 256.
+        strength: The strength of the mnemonic in bits. Can be 128 for 12 words or 256 for 24 words.
 
     Returns:
         A string containing the mnemonic.
